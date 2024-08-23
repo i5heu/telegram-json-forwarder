f2d6563b86fd1a89f1ccd1a25d9d51a990b4277e
i5heu <i5heu@i5h.eu>
2024-08-23 16:15:49 +0000

    Start server on port 80 instead of 8080

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ func main() {
 
 	http.HandleFunc("/webhook", webhookHandler)
 
-	// Start the server on port 8080
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Start the server on port 80
+	log.Println("Starting server on :80")
+	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
 	}
 }
